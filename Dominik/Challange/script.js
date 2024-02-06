2e80cd7dc9d21800e8a5ac639e374b5c104ef0be
Dominik Burycz <dominik.burycz@gmail.com>
2024-02-06 15:40:27 +0100

    added different cars

--- Dominik/Challange/script.js
+++ Dominik/Challange/script.js
@@ -25,9 +25,9 @@ let car1 = new Cars("/Dominik/Challange/pics/corvette-4535949_640.jpg", "Chevrol
 
 let car2 = new Cars("/Dominik/Challange/pics/ford-64041_640.jpg", "Ford Mustang", "The Ford Mustang is a series of American automobiles manufactured by Ford.")
 
-let car3 = new Cars("/Dominik/Challange/pics/ford-64041_640.jpg", "Ford Mustang", "The Ford Mustang is a series of American automobiles manufactured by Ford.")
+let car3 = new Cars("https://live.staticflickr.com/4527/27180567849_49dd8635bf_b.jpg", "Lexus LC500", "The Lexus LC is a grand tourer manufactured by Lexus, a luxury division of Toyota.")
 
-let car4 = new Cars("/Dominik/Challange/pics/ford-64041_640.jpg", "Ford Mustang", "The Ford Mustang is a series of American automobiles manufactured by Ford.")
+let car4 = new Cars("https://live.staticflickr.com/4200/35181144841_b3d1cf9789_b.jpg", "Honda NSX", "Marketed in North America and Hong Kong as the Acura NSX is a 2-seater, mid-engine sports car that was manufactured by Honda in Japan from 1990 until 2005.")
 
 let car5 = new Cars("/Dominik/Challange/pics/ford-64041_640.jpg", "Ford Mustang", "The Ford Mustang is a series of American automobiles manufactured by Ford.")
 
